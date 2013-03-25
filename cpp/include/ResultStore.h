7d3726d113ba18ffd65e20c8786b994c60794fa3
Daniel Damiani <dsdamiani@gmail.com>
2013-03-25 01:08:56 -0700

    Inline getters and setters in ResultStore class

--- cpp/include/ResultStore.h
+++ cpp/include/ResultStore.h
@@ -5,7 +5,7 @@ class ResultStore {
  public:
 
   ResultStore();
-  ResultStore(double sampling_point);
+  explicit ResultStore(double sampling_point);
   virtual ~ResultStore();
 
   double GetSamplingPoint() const;
@@ -26,5 +26,34 @@ class ResultStore {
   int m_num_charged;
   int m_num_neutral;
 };
+
+inline double ResultStore::GetSamplingPoint() const {
+  return m_sample_point;
+}
+
+inline int ResultStore::GetTotalNumParticles() const {
+  return m_num_charged + m_num_neutral;
+}
+
+inline int ResultStore::GetNumChargedParticles() const {
+  return m_num_charged;
+}
+
+inline int ResultStore::GetNumNeutralParticles() const {
+  return m_num_neutral;
+}
+
+inline void ResultStore::IncrementNumChargedParticles() {
+  m_num_charged++;
+}
+
+inline void ResultStore::IncrementNumNeutralParticles() {
+  m_num_neutral++;
+}
+
+inline void ResultStore::ResetCounts() {
+  m_num_charged = 0;
+  m_num_neutral = 0;
+}
 
 #endif
--- cpp/src/ResultStore.cpp
+++ cpp/src/ResultStore.cpp
@@ -13,32 +13,3 @@ ResultStore::ResultStore(double sampling_point) :
 {}
 
 ResultStore::~ResultStore() {}
-
-double ResultStore::GetSamplingPoint() const {
-  return m_sample_point;
-}
-
-int ResultStore::GetTotalNumParticles() const {
-  return m_num_charged + m_num_neutral;
-}
-
-int ResultStore::GetNumChargedParticles() const {
-  return m_num_charged;
-}
-
-int ResultStore::GetNumNeutralParticles() const {
-  return m_num_neutral;
-}
-
-void ResultStore::IncrementNumChargedParticles() {
-  m_num_charged++;
-}
-
-void ResultStore::IncrementNumNeutralParticles() {
-  m_num_neutral++;
-}
-
-void ResultStore::ResetCounts() {
-  m_num_charged = 0;
-  m_num_neutral = 0;
-}
