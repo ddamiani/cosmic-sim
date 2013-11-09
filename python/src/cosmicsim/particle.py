009324fdb808a83f7fad9bc11629b490523ff5d4
Daniel Damiani <ddamiani@slac.stanford.edu>
2013-11-09 01:57:51 -0800

    Add base particle class

--- python/src/cosmicsim/particle.py
+++ python/src/cosmicsim/particle.py
@@ -1,10 +1,20 @@
 class Particle:
-    '''
+    """
     A class representing a particle
-    '''
+    """
 
-    def __init__(self):
+    def __init__(self,
+                energy,
+                position,
+                rad_length,
+                term_dist,
+                parent=None,
+                results=None):
         self.energy = energy
-        self.positon = position
-        self.particle_id = particle
-        self.parent_id = parent_id
+        self.position = position
+        self.rad_length = rad_length
+        self.term_distance = term_dist
+        self.parent = parent
+        self.results = results
+        self.child_1 = None
+        self.child_2 = None
